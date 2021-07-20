741f29bece70293c2520c53cb97ec0f1fc438a51
Matheus Marinho Feitoza <feitozamatheus123@gmail.com>
2021-07-20 12:19:50 -0300

    :lipstick: Add a new component style for boardGame

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,10 +1,12 @@
-import './style.css'
+import './style.css';
 import CardGame from "../../components/CardGame";
 
 function BoardGame(amountCards){
     let $htmlBoardCardGame = CardGame().repeat(amountCards);
     return`
+        <section class="board-game">
         ${$htmlBoardCardGame}
+        </section>
     `
 }
 export default BoardGame;
